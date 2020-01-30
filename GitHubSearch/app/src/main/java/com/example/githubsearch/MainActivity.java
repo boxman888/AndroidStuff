83dd46e2930bd87cf02673cbd95c5ed045ebc940
boxman888 <epatterson888@hotmail.com>
2020-01-29 19:23:44 -0800

    fixed the layouts, added an adapte, and modified the main activity

--- GitHubSearch/app/src/main/java/com/example/githubsearch/MainActivity.java
+++ GitHubSearch/app/src/main/java/com/example/githubsearch/MainActivity.java
@@ -1,21 +1,68 @@
 package com.example.githubsearch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private RecyclerView mSearchResultsRV;
+    private EditText mSearchBoxET;
+    private GitHubSearchAdapter mGitHubSearchAdapter;
+
+    private String[] dummySearchResults = {
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+            "Dummy search results",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSearchBoxET = findViewById(R.id.et_search_box);
+        mSearchResultsRV = findViewById(R.id.rv_search_results);
+
+        mSearchResultsRV.setLayoutManager(new LinearLayoutManager(this));
+        mSearchResultsRV.setHasFixedSize(true);
+
+        mGitHubSearchAdapter = new GitHubSearchAdapter();
+        mSearchResultsRV.setAdapter(mGitHubSearchAdapter);
+
+        Button searchButton = findViewById(R.id.btn_search);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchQuery = mSearchBoxET.getText().toString();
+                if (!TextUtils.isEmpty(searchQuery)) {
+                    mGitHubSearchAdapter.updateSearchResults(new ArrayList<String>(Arrays.asList(dummySearchResults)));
+                    String url = GitHubUtils.buildGitHubSearchURL(searchQuery);
+                    Log.d(TAG, "querying url: " + url);
+                    mSearchBoxET.setText("");
+                }
+            }
+        });
     }
 
     private String doGitHubSearch(String query){
