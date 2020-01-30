d008539c48b8ee8ff7cf003dc8a610d08941527f
boxman888 <epatterson888@hotmail.com>
2020-01-30 15:09:16 -0800

    Returns git response.

--- GitHubSearch/app/src/main/java/com/example/githubsearch/GitHubUtils.java
+++ GitHubSearch/app/src/main/java/com/example/githubsearch/GitHubUtils.java
@@ -3,7 +3,7 @@ package com.example.githubsearch;
 import android.net.Uri;
 
 public class GitHubUtils {
-    private final static String GITHUB_SEARCH_BASE_URL = "https:://api.github.com/search/repositories";
+    private final static String GITHUB_SEARCH_BASE_URL = "http:://api.github.com/search/repositories";
     private final static String GITHUB_SEARCH_QUERY_PARAM = "q";
     private final static String GITHUB_SEARCH_SORT_PARAM = "sort";
     private final static String GITHUB_SEARCH_SORT_VALUE = "stars";
--- GitHubSearch/app/src/main/java/com/example/githubsearch/MainActivity.java
+++ GitHubSearch/app/src/main/java/com/example/githubsearch/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -12,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -56,25 +58,41 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String searchQuery = mSearchBoxET.getText().toString();
                 if (!TextUtils.isEmpty(searchQuery)) {
-                    mGitHubSearchAdapter.updateSearchResults(new ArrayList<String>(Arrays.asList(dummySearchResults)));
-                    String url = GitHubUtils.buildGitHubSearchURL(searchQuery);
-                    Log.d(TAG, "querying url: " + url);
-                    mSearchBoxET.setText("");
+                    doGitHubSearch(searchQuery);
                 }
             }
         });
     }
 
-    private String doGitHubSearch(String query){
+    private void doGitHubSearch(String query){
          String url = GitHubUtils.buildGitHubSearchURL(query);
          Log.d(TAG, "querying search URL: " + url);
+         new GitHubSearchTask().execute(url);
+    }
 
-         String results = null;
-         try{
-             results = NetworkUtils.doHTTPGet(url);
-         } catch (IOException e){
-             e.printStackTrace();
+    public class GitHubSearchTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String url = strings[0];
+            String results = null;
+            try{
+                results = NetworkUtils.doHTTPGet(url);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return results;
         }
-        return results;
+
+        @Override
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+            if (s != null){
+                ArrayList<String> searchResultsList = new ArrayList<>();
+                searchResultsList.add(s);
+                mGitHubSearchAdapter.updateSearchResults(new ArrayList<String>(Arrays.asList(dummySearchResults)));
+                mSearchBoxET.setText("");
+            }
+        }
     }
 }
